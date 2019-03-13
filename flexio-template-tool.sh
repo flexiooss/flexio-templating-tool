2462151b48d6e353558eb1fa8fe41deec6b609b7
Thibaud Jeannin <thibaud.jeannin@gmail.com>
2019-03-13 11:26:15 +0100

    retiré print

--- flexio-template-tool.sh
+++ flexio-template-tool.sh
@@ -40,7 +40,6 @@ case ${key} in
 esac
 done
 
-echo ${args[@]}
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 source ${SCRIPT_DIR}/venv/bin/activate
 python3.7 ${SCRIPT_DIR}/src/main.py "${args[@]}" --out ${OUTPUT_DIRECTORY}
