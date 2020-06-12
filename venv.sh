3fb941b29c69f463bcc8049395ec4dae80dbc5bb
Thibaud Jeannin <thibaud.jeannin@gmail.com>
2020-06-12 16:49:17 +0200

    Fix venv.sh

--- venv.sh
+++ venv.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 set -e
+
+CURRENT_PWD=$PWD
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+
 if [ ! -f /usr/bin/python3.8 ]; then
     echo "Python 3.8 is required"
     exit 1
 fi
-python3.8 -m venv $PWD/venv
-source $PWD/venv/bin/activate
+
+cd ${SCRIPT_DIR}
+
+python3.8 -m venv ${SCRIPT_DIR}/venv
+source ${SCRIPT_DIR}/venv/bin/activate
 python3.8 -m pip install --upgrade pip
 
 set +e
-python3.8 -m pip install -r requirements.txt
+python3.8 -m pip install -r ${SCRIPT_DIR}/requirements.txt
+
+cd ${CURRENT_PWD}
